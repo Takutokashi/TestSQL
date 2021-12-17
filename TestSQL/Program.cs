fc2616c657d763a50b29c1e6b710c4d62a39a161
Computer <Computer@HPALBERT>
2021-12-17 16:08:38 +0300

    Добавьте файлы проекта.

---
+++ TestSQL/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TestSQL.HelloApp;
+
+namespace TestSQL
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    namespace HelloApp
+    {
+        public class ApplicationContext : DbContext
+        {
+            public DbSet<User> Users { get; set; }
+
+            public ApplicationContext()
+            {
+                Database.EnsureCreated();
+            }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext()) 
+            {
+                // создаем два объекта User
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                // добавляем их в бд
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+        }
+    }
+}
